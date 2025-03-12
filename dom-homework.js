015bc5e51becdca657a031ff84b9369609dbd670
Nmpatel133 <nmpatel133@gmail.com>
2025-03-12 14:02:40 -0400

    pushing some stuff

---
+++ dom-homework.js
@@ -0,0 +1,36 @@
+const divOne = document.getElementById('myDiv');
+
+let headerOne = document.createElement('h1');
+headerOne.innerHTML = 'Welcome to DOM homework';
+divOne.appendChild(headerOne);
+
+
+let paragraphOne = document.createElement('p');
+paragraphOne.innerHTML = 'This is your first DOM homework assignment';
+divOne.appendChild(paragraphOne);
+
+
+let unorderedList = document.createElement('ul');
+unorderedList.innerHTML = 'List of Fruits'
+let liOne = document.createElement('li');
+liOne.innerHTML = 'Apple';
+let liTwo = document.createElement('li');
+liTwo.innerHTML = 'Banana';
+let liThree = document.createElement('li');
+liThree.innerHTML = 'Orange';
+unorderedList.appendChild(liOne)
+unorderedList.appendChild(liTwo)
+unorderedList.appendChild(liThree)
+divOne.appendChild(unorderedList);
+
+headerOne.setAttribute('class', 'highlight');
+
+let buttonOne = document.createElement('button');
+buttonOne.innerHTML = 'Add new list item';
+divOne.appendChild(buttonOne);
+
+buttonOne.addEventListener('click', function() {
+    let newListItem = document.createElement('li');
+    newListItem.innerHTML = 'New List Item ';
+    unorderedList.appendChild(newListItem);
+})
